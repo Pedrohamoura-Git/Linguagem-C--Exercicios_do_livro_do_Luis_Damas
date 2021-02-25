47dbf5e2b2f639fb19c063df1adb4d7c0163c30a
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2021-02-25 09:25:05 -0300

    Lista 1 - Estrutura de controle
    Lista de exercícios encontrada na Internet para complementar os meus estudos.

---
+++ Cap 04/Lista 1 - Estrutura de Controle/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n\n");
+
+    int num, cont, soma, produto;
+
+    printf("Insira um numero inteiro: ");
+    scanf("%d", &num);
+
+    for (num != 0, soma = 0, produto = 1, cont = 1; cont <= num; cont ++)
+    {
+        soma = soma + cont;
+        produto = produto * cont;
+        printf("%d\\%d\\%d\\%d\n\n", num, cont, soma, produto);
+    }
+
+    return 0;
+}
+

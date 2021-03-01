99ecfb39eaffd63b5b4d7863e21549a933d8e183
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2021-03-01 10:18:14 -0300

    Exercício0712
    ----------------- DESCRIÇÃO DA QUESTÃO ------------------------------------------------------------------------------------------------------
    
     Implemente a função abaixo que apaga todas as ocorrências do caractere ch na string s.
    
            char *strdelc(char *s, char ch)
    
     (!)Nota: Apagar um caractere não é substituí-lo por um espaço em branco, mas retirá-lo
    efetivamente da string.
    
    Funcionando?
        Sim - ()
        Não - (X)
    ----------------- COMENTÁRIOS ---------------------------------------------------------------------------------------------------------------
    //(1)//
     Por algum motivo o loop para de funcionar quando o código s[j] = s[j+1]; é executado.
    Sem ele todas as repetições são realizadas normalmente.
    
    //(2)//
     "..."

---
+++ Cap 07/Exercício0712/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/*
+----------------- DESCRIÇÃO DA QUESTÃO ------------------------------------------------------------------------------------------------------
+
+ Implemente a função abaixo que apaga todas as ocorrências do caractere ch na string s.
+
+        char *strdelc(char *s, char ch)
+
+ (!)Nota: Apagar um caractere não é substituí-lo por um espaço em branco, mas retirá-lo
+efetivamente da string.
+
+
+Funcionando?
+    Sim - ()
+    Não - (X)
+----------------- COMENTÁRIOS ---------------------------------------------------------------------------------------------------------------
+//(1)//
+ Por algum motivo o loop para de funcionar quando o código s[j] = s[j+1]; é executado.
+Sem ele todas as repetições são realizadas normalmente.
+
+//(2)//
+ "..."
+
+
+---------------- PROTÓTIPOS DAS FUNÇÕES -----------------------------------------------------------------------------------------------------
+*/
+
+char *strdelc(char *s, char ch);
+//------------------------------------- -----------------------------------------------------------------------------------------------------
+int main()
+{
+    printf("Hello world!\n\n");
+
+//------ Definindo elementos
+    char *s = "012345";
+    char ch;
+
+//------ Armazenando o input do usuário
+    printf("Dada a string: \"%s\"\nInsira qual caractere voce deseja remover: ", s);
+    scanf(" %c", &ch);
+
+//------ Imprimindo o resultado
+    strdelc(s, ch);
+
+//------ The End...?
+    return 0;
+}
+//-------------- FUNÇÕES ---------------------------------------------------------------------------------------------------------------------
+
+char *strdelc(char *s, char ch)
+{
+    int i, j, tam = strlen(s);
+    //printf("tam: %d\n", tam); //Debug tam
+//Loop para procurar o ch dentro da *s;
+    for(i = 0; i < tam; i++)
+    {
+        //printf("i: %d\n", i); //Debug i
+        if(s[i] == ch)
+        {
+            j = i;
+            printf("j: %d\n", j); //Debug j
+            printf("s[%d] = s[%d+1] --> %c\n", j, j, s[j+1]);
+            //(1)//s[j] = s[j+1];
+        }
+    }
+    return s;
+}
+
+
